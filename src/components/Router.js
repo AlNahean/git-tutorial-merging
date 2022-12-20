07768e7893b0df8f31aa93424529b45904975207
AlNahean <nahean.fardous@gmail.com>
2022-12-20 22:01:15 +0600

    feature-one

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,8 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Page/Home/home";
 import Error from "./Page/Error/Error";
-
-
+import FeatireOne from "./Page/FratureOne/FeatireOne";
 
 const Routing = () => {
   return (
@@ -11,6 +10,11 @@ const Routing = () => {
       <Routes>
         <Route path="/" caseSensitive={false} element={<Home />} />
         <Route path="/home" caseSensitive={false} element={<Home />} />
+        <Route
+          path="/feature-one"
+          caseSensitive={false}
+          element={<FeatireOne />}
+        />
         <Route path="*" caseSensitive={false} element={<Error />} />
       </Routes>
     </Router>
