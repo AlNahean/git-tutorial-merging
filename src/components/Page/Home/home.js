e408c22fce7d97d1c7c0fae679b075c60c9e47be
AlNahean <nahean.fardous@gmail.com>
2022-12-20 20:46:36 +0600

    first commit

---
+++ src/components/Page/Home/home.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import { useGlobalContext } from "../../Context";
+
+import "./Home.scss"
+
+const Home = () => {
+  const { test } = useGlobalContext();
+
+  return <div>
+    Home
+  </div>;
+};
+
+export default Home;
